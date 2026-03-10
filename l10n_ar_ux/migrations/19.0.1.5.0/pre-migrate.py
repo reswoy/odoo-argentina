f2f6dc112c84f740807eaadcc4ffa3e3f68247c2
Giuliano (Relex) <giuliano@relex.ar>
2026-03-10 09:45:44 -0300

    [MIG] l10n_ar_ux: fix pre-migrate to handle JSONB arch_db in Odoo 19

--- l10n_ar_ux/migrations/19.0.1.5.0/pre-migrate.py
+++ l10n_ar_ux/migrations/19.0.1.5.0/pre-migrate.py
@@ -5,14 +5,23 @@ In Odoo 19, the field l10n_ar_afip_activity_id and the model l10n_ar.afip.activi
 were removed from l10n_ar core. The platform upgrade does not clean up inherited
 views that reference this field, causing a ParseError during view validation.
 """
-from odoo.tools import sql
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
+    # arch_db is stored as JSONB in Odoo 19 (translatable field),
+    # so we cast to text for LIKE to work
     cr.execute("""
         DELETE FROM ir_ui_view
         WHERE model = 'res.config.settings'
-        AND arch_db LIKE '%%l10n_ar_afip_activity_id%%'
+        AND arch_db::text LIKE '%%l10n_ar_afip_activity_id%%'
     """)
     if cr.rowcount:
-        cr.execute("DELETE FROM ir_model_data WHERE model = 'ir.ui.view' AND res_id NOT IN (SELECT id FROM ir_ui_view)")
+        _logger.info("Deleted %s orphan view(s) referencing l10n_ar_afip_activity_id", cr.rowcount)
+        cr.execute("""
+            DELETE FROM ir_model_data
+            WHERE model = 'ir.ui.view'
+            AND res_id NOT IN (SELECT id FROM ir_ui_view)
+        """)
